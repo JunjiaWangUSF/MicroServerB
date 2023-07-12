f878af1aeac4be66ca957f10ffd78115298ce697
Junjia Wang <jwang268@dons.usfca.edu>
2023-07-12 17:10:12 -0400

    init

---
+++ src/main/java/com/FortonitoMicroServerB/mockServerB/MockServerBApplication.java
@@ -0,0 +1,13 @@
+package com.FortonitoMicroServerB.mockServerB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockServerBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockServerBApplication.class, args);
+	}
+
+}
